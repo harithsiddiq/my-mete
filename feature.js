2fdfac478b3d6ee6f1890ac8269ca8678224f369
harithsiddiq <hind1123.ih@gmail.com>
2022-09-29 07:08:26 +0200

    added new feature2 !!

--- feature.js
+++ feature.js
@@ -2,7 +2,7 @@ let destance = function(mails) {
     if(mails > 20) {
         return 'Call Upper'
     } else {
-        return 'Use Transport uh'
+        return 'Use Transport or walk'
     }
 }
 
